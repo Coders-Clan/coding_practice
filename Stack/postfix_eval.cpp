f183040de31dd2dd54fa16cab3403abb7a62b909
Ankita Puri <54734002+ankitapuri@users.noreply.github.com>
2020-11-11 23:35:59 +0530

    Create postfix_eval.cpp

---
+++ Stack/postfix_eval.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+# define MAX 50
+class Stack{
+	int top;
+public:
+    char arr[MAX];
+	Stack(){top=-1;}
+
+	char push(char item)
+	{
+		if(top>=(MAX-1))
+		 cout<<"Stack overflow";
+		else
+		 arr[++top]=item;
+	}
+	char pop()
+	{
+		if(top<0)
+		 cout<<"Stack underflow";
+		else
+		{
+		 int item=arr[top--];
+		 return item;
+		}
+	}
+};	
+	int main()
+	{
+		Stack s;
+		char str[50];
+		gets(str);
+		for(int i=0;i<strlen(str);i++)
+		  s.push(str[i]);
+		for(int i=0;i<strlen(str);i++)  
+		  str[i]=s.pop();
+	cout<<"The reverse string is:";
+	puts(str);
+	return 0;
+	}
